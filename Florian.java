b12ab0e0975c9871a18ee8aed025342afcbeabb1
FlorianBunjaku <58727600+FlorianBunjaku@users.noreply.github.com>
2020-03-29 14:10:35 +0200

    Update Florian.java

--- Florian.java
+++ Florian.java
@@ -2,16 +2,12 @@ import java.util.Scanner;
 
 public class Florian {
     public static void main(String[] args) {
-        System.out.println("Shenoni mesazhin:");
         Scanner input=new Scanner(System.in);
         int x=1;
-        do {
+        while(x==1) {
             try {
-
-
+                System.out.println("Shenoni mesazhin:");
                 String mesazhi = input.nextLine();
-
-
                 String encrypted = encryptMessage(mesazhi);
                 String decrypted = decryptMessage(encrypted);
                 System.out.println("Teksti i enkriptuar eshte:"+encrypted);
@@ -19,8 +15,9 @@ public class Florian {
                 x=2;
             } catch (Exception e) {
                 System.out.println("Keni vendosur nje tekst jo valid!Provoni perseri");
+
             }
-        }while (x==1);
+        }
     }
 
     private static String encryptMessage(String message) {
